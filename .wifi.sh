d1be543a5d8c3331bb55f7e7cc8f518d0108035d
Redson dos Santos Silva <61910521+RedsonBr140@users.noreply.github.com>
2021-06-07 21:04:02 -0300

    Update .wifi.sh

--- .wifi.sh
+++ .wifi.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+# Script que mostra sua rede wifi para a barra do dwm, pode ser necessário editar dependendo da placa de rede.
 ssidnt="$(iw wlan0 info | sed -n '5p' | sed -e 's/\<ssid\>//g')"
 ssidt="$(echo $ssidnt | sed -e 's/\s+//g')"
 
